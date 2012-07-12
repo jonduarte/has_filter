84b4a0cc6e1632f04c28dc3dc357b937cab290e8
Jonthan Duarte <jonathan.duarte@rocketmail.com>
2012-07-11 22:13:14 -0300

    Use bind assignment to compatibility with rails 2.3

--- lib/has_filter.rb
+++ lib/has_filter.rb
@@ -10,42 +10,67 @@ module HasFilter
       @allowed_fields = *allowed_fields
     end
 
-    def filtering(conditions)
+    def filtering(filtering)
+      conditions = filtering
       filters = []
+
       if @allowed_fields.present?
         conditions = conditions.select { |key| @allowed_fields.include? key }
-        if conditions.empty?
-          filters << hash_conditions(:id, nil)
-        end
-        conditions.each do |key, value|
-          next if value.nil?
-          if self.columns_hash[key.to_s].type == :string
-            filters << like_conditions(key, value)
-          else
-            filters << hash_conditions(key, value)
-          end
-        end
-      else
-        conditions.each do |key, value|
-          next if value.nil?
-          if self.columns_hash[key.to_s].type == :string
-            filters << like_conditions(key, value)
-          else
-            filters << hash_conditions(key, value)
-          end
-        end
       end
-      find(:all, *filters)
+
+      if conditions.empty?
+        filters << hash_conditions(:id, nil)
+      else
+        filters << set_filters(conditions)
+      end
+
+      filters = filters.flatten
+      find(:all, :conditions => filters)
     end
 
     private
 
     def hash_conditions(key, value)
-      { :conditions => { key => value } }
+      if value.is_a? Array
+        ["#{key.to_s} in (:#{key.to_s})"]
+      else
+        ["#{key.to_s} = :#{key.to_s}"]
+      end
     end
 
     def like_conditions(key, value)
-      { :conditions => ["#{key.to_s} like ?", "%#{value}%"] }
+      if value.is_a? Array
+        ["#{key.to_s} in (:#{key.to_s})"]
+      else
+        ["#{key.to_s} like :#{key.to_s}"]
+      end
+    end
+
+    def set_filters(filtering)
+      conditions = filtering
+      filters = []
+
+      conditions.reject! { |k, v| v.nil? }
+
+      conditions.each do |key, value|
+        next if value.nil?
+        if self.columns_hash[key.to_s].type == :string
+          filters << like_conditions(key, value)
+        else
+          filters << hash_conditions(key, value)
+        end
+      end
+
+      conditions = conditions.inject({}) do |h, (k, v)|
+        if !h[k].is_a?(Array) && self.columns_hash[k.to_s].type == :string
+          h[k] = "%#{v}%"
+        else
+          h[k] = v
+        end
+        h
+      end
+
+      [filters.join(" AND "), conditions]
     end
   end
 end
--- test/has_filter_test.rb
+++ test/has_filter_test.rb
@@ -24,6 +24,23 @@ class Post < ActiveRecord::Base
   has_filter :title
 end
 
+class Activity < ActiveRecord::Base
+  establish_connection :adapter => 'sqlite3', :database => ':memory:'
+  connection.create_table table_name do |t|
+    t.integer :category_id
+    t.integer :school_grade_id
+    t.integer :discipline_id
+    t.integer :custom_theme_id
+    t.integer :custom_subject_id
+    t.string  :objective
+    t.string  :pcn
+    t.string  :name
+    t.string  :status
+  end
+
+  has_filter
+end
+
 class HasFilterTest < ActiveSupport::TestCase
   test "create an article instance" do
     article = Article.create(:title => "Foo", :content => "Bar", :active => true, :category_id => 1)
@@ -99,5 +116,40 @@ class HasFilterTest < ActiveSupport::TestCase
 
     articles = Article.filtering(:active => [true, false])
     assert_equal 3, articles.size
+  end
+
+  test "multiple array conditions" do
+    activities = []
+    2.times do
+      activities << Activity.create(:school_grade_id   => 1,
+                      :discipline_id     => 1,
+                      :custom_theme_id   => 1,
+                      :custom_subject_id => 1,
+                      :objective         => "some objetive",
+                      :pcn               => "some pcn",
+                      :name              => nil,
+                      :status            => "inactive")
+    end
+
+    Activity.create(:school_grade_id   => 3,
+                    :discipline_id     => 1,
+                    :custom_theme_id   => 1,
+                    :custom_subject_id => 1,
+                    :objective         => "some objetive",
+                    :pcn               => "some pcn",
+                    :name              => nil,
+                    :status            => "inactive")
+
+    multiple_conditions = {:custom_theme_id   => 1,
+                           :pcn               => "some pcn",
+                           :school_grade_id   => 1,
+                           :objective         => "some objetive",
+                           :custom_subject_id => 1,
+                           :status            => "inactive",
+                           :discipline_id     => 1 }
+
+    results = Activity.filtering(multiple_conditions)
+    assert_equal 2, results.size
+    assert_equal results, activities
   end
 end
